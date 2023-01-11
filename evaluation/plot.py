f05c7e31a4ac51dcd2eb499042b0b5af7d2e7b4e
mathi <mathias_a_sandnes@hotmail.com>
2023-01-11 13:17:04 +0100

    Added new result plotting

--- evaluation/plot.py
+++ evaluation/plot.py
@@ -6,39 +6,33 @@ import seaborn as sns
 import numpy as np
 
 
-def plot_results(file_name):
+def plot_results(file_names):
     window_size = 5
     sns.set_style('white')
     sns.set_context("paper", font_scale=1)
 
-    with open(f'../results/{file_name}', 'r') as f:
-        r = json.load(f)
+    for i, file_name in enumerate(file_names):
+        with open(f'../results/{file_name}', 'r') as f:
+            r = json.load(f)
 
-    results = [np.average(x['simple_eval_scores']) for x in r['results']]
+        results = [np.average(x['simple_eval_scores']) for x in r['results']]
+        print(i, r['hparams'], max(results))
+        plt.plot(results, label=i)
 
-    dev_note = r['note']
-    # r = pd.json_normalize(r['results'])
-    # r['rolling_average'] = r['results'].rolling(window=window_size).mean()
-
-    plt.plot(results, label='Score')
-    ax = sns.lineplot(results)
-    ax.axhline(200, color='red')
-    ax.axhline(0, color='green')
-    # plt.ylim(-2000, 400)
-    plt.legend(loc='upper right', labels=['Score', 'Success'])
-    ax.set(title=dev_note)
+    ax = plt.gca()
+    ax.axhline(200, color='red', label='Success')
+    ax.axhline(0, color='green', label='Failure')
+    plt.legend(loc='upper right')
     ax.set(xlabel='Episodes', ylabel='Score')
-    leg = ax.get_legend()
-    leg.legendHandles[1].set_color('red')
+    plt.title("Multiple Line Plot")
     plt.show()
-    fig = ax.get_figure()
-
     if not os.path.isdir('plots'):
         os.mkdir('plots')
 
-    fig.savefig(f'plots/{dev_note}.png')
+    fig = ax.get_figure()
+    fig.savefig(f'plots/multiple_lines.png')
 
 
 if __name__ == '__main__':
-    file_name = os.listdir('../results')[-1]  # Plot the latest result
-    plot_results(file_name)
+    file_names = os.listdir('../results')
+    plot_results(file_names)
